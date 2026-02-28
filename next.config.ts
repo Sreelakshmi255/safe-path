ecffc38005303f249aaa2e7850087b4068b3be41
Sreelakshmi255 <tmsreelakshmi916@gmail.com>
2026-02-28 11:24:43 +0530

    ghbhjb

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  compress: true,
+  // This suppresses favicon 404 warnings
+  async generateBuildId() {
+    return 'build'
+  },
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
-};
-
-export default nextConfig;
+export default nextConfig
